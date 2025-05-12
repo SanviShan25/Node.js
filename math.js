2db9aee21e12e6f4b995153e33880baf31ee7e45
S2506 <sanvishan21@gmail.com>
2025-05-12 17:59:03 +0530

    initial commit

---
+++ math.js
@@ -0,0 +1,39 @@
+//METHOD 1
+// module.exports.sum=(a,b)=>a+b;
+// const sum =(a,b)=> a+b;
+// const mul =(a,b)=> a*b;
+// const g=9.8;
+// const PI=3.14;
+// module.exports=123;
+
+//METHOD 2
+// let obj={
+//         sum:sum,
+//         mul: mul,
+//         g: g,
+//         PI: PI,
+// };
+// module.exports=obj;
+
+//METHOD 3
+// module.exports.sum=(a,b)=>a+b;
+// module.exports.sum =(a,b)=> a+b;
+// module.exports.mul =(a,b)=> a*b;
+// module.exports.g=9.8;
+// module.exports.PI=3.14;
+
+//METHOD 4
+// exports.sum=(a,b)=>a+b;
+// exports.sum =(a,b)=> a+b;
+// exports.mul =(a,b)=> a*b;
+// exports.g=9.8;
+// exports.PI=3.14;
+
+
+//IMPORT MODULES
+// export const sum =(a,b)=> a+b;
+// export const mul =(a,b)=> a*b;
+// export const g=9.8;
+// export const PI=3.14;
+
+
---
+++ script.js
@@ -0,0 +1,49 @@
+//NODE FILES
+// let n=5;
+// for (let i=0; i<n; i++){
+//     console.log("hello, " +i);
+// }
+
+
+//PROCESS IN NODE
+// console.log(process.argv);
+// let args= process.argv;
+// for (let i=2; i<args.length; i++){
+//     console.log("hello to",args[i]);
+// }
+
+
+//EXPORT IN FILES
+// const someValue=require("./math");
+// console.log(someValue);
+
+// const math=require("./math");
+// // console.log(math.sum);
+// console.log(math.sum (2,2));
+
+
+//EXPORT IN DIRECTORIES
+// const info =require("./Fruits");
+// console.log(info);
+// console.log(info[0]);
+
+
+//LOCAL VS GLOBAL INSTALLATION
+// const figlet= require("figlet");
+// figlet("Sanvi Shan", function (err, data) {
+//     if (err) {
+//       console.log("Something went wrong...");
+//       console.dir(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+
+
+//IMPORT MODULES
+// // import {sum, PI}from "./math.js";
+
+// import { generate, count } from "random-words";
+
+// // console.log(PI);
+// console.log(generate());
